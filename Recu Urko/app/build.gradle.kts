ea18a45bf6ae414ac6b8821a19f256dc6e31f461
garcia.aner <148456952+AnerGarcia@users.noreply.github.com>
2025-05-26 19:22:02 +0200

    .

--- Recu Urko/app/build.gradle.kts
+++ Recu Urko/app/build.gradle.kts
@@ -1,8 +1,6 @@
-
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    id("kotlin-kapt")
 }
 
 android {
@@ -15,6 +13,7 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
+
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -28,57 +27,44 @@ android {
         }
     }
 
-    buildFeatures {
-        viewBinding = true
-    }
+    buildFeatures{
 
+        viewBinding=true
+
+
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-
     kotlinOptions {
         jvmTarget = "11"
     }
 }
 
 dependencies {
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.fragment.ktx)
 
-    // ViewModel y LiveData
+
+    //viewModel
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.0")
+
+    //liveData
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
+    //glide
+    implementation ("com.github.bumptech.glide:glide:4.15.1")
 
-    // Room Database
-    implementation("androidx.room:room-runtime:2.6.1")
-    implementation("androidx.room:room-ktx:2.6.1")
-    kapt("androidx.room:room-compiler:2.6.1")
-
-    // Retrofit
+    //retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
-    // Glide
-    implementation("com.github.bumptech.glide:glide:4.15.1")
 
-    // Firebase
-    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
-    implementation("com.google.firebase:firebase-firestore")
-    implementation("com.google.firebase:firebase-auth")
-
-    // Supabase
-    implementation("io.github.jan-tennert.supabase:postgrest-kt:2.0.4")
-    implementation("io.github.jan-tennert.supabase:realtime-kt:2.0.4")
-
-    // Navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
